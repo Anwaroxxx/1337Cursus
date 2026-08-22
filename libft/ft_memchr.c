c8feb9bdad1c6dd1f83f3d00448443f606901571
Anwaroxxx <anwar.azarzou.6969@gmail.com>
2026-08-22 23:37:05 +0100

    libft: add ft_memchr

---
+++ libft/ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   ft_memchr.c                                         +:      :+:    :+:   */
+/*                                                       +:+ +:+         +:+  */
+/*   By: anwar <anwar@student.42.fr>                     +#+  +:+       +#+   */
+/*                                                       +#+#+#+#+#+   +#+    */
+/*   Created: 2026/08/22 23:37:05 by anwar               #+#+    #+#          */
+/*   Updated: 2026/08/22 23:37:05 by anwar               ###   ########.fr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*p;
+
+	p = (const unsigned char *)s;
+	while (n--)
+	{
+		if (*p == (unsigned char)c)
+			return ((void *)p);
+		p++;
+	}
+	return (NULL);
+}
