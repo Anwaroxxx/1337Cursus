79640be1ae6640234143f095332e0a33d2553a46
Anwaroxxx <anwar.azarzou.6969@gmail.com>
2026-08-22 23:42:54 +0100

    libft: add missing ft_tolower/ft_toupper prototypes

--- libft/libft.h
+++ libft/libft.h
@@ -28,6 +28,8 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 size_t	ft_strlen(const char *s);
 
 void	*ft_memset(void *b, int c, size_t len);
