1f8ce746a49783c214d8faa4069930d6c6a52032
Anwaroxxx <anwar.azarzou.6969@gmail.com>
2026-08-22 23:38:31 +0100

    libft: add ft_strmapi

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   ft_strmapi.c                                        +:      :+:    :+:   */
+/*                                                       +:+ +:+         +:+  */
+/*   By: anwar <anwar@student.42.fr>                     +#+  +:+       +#+   */
+/*                                                       +#+#+#+#+#+   +#+    */
+/*   Created: 2026/08/22 23:38:31 by anwar               #+#+    #+#          */
+/*   Updated: 2026/08/22 23:38:31 by anwar               ###   ########.fr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+	char	*res;
+	size_t	i;
+
+	if (!s || !f)
+		return (NULL);
+	res = (char *)malloc(ft_strlen(s) + 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		res[i] = f(i, s[i]);
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
