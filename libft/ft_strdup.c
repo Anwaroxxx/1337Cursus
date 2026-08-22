d3368821bb1a068080daf859c12c6d6765fa8744
Anwaroxxx <anwar.azarzou.6969@gmail.com>
2026-08-22 23:36:33 +0100

    libft: add ft_strdup

---
+++ libft/ft_strdup.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   ft_strdup.c                                         +:      :+:    :+:   */
+/*                                                       +:+ +:+         +:+  */
+/*   By: anwar <anwar@student.42.fr>                     +#+  +:+       +#+   */
+/*                                                       +#+#+#+#+#+   +#+    */
+/*   Created: 2026/08/22 23:36:33 by anwar               #+#+    #+#          */
+/*   Updated: 2026/08/22 23:36:33 by anwar               ###   ########.fr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dup;
+	size_t	len;
+
+	len = ft_strlen(s1) + 1;
+	dup = (char *)malloc(len);
+	if (!dup)
+		return (NULL);
+	ft_memcpy(dup, s1, len);
+	return (dup);
+}
