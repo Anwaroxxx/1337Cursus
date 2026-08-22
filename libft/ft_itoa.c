b998184f58cfe3bf84143f41b69d848640480e19
Anwaroxxx <anwar.azarzou.6969@gmail.com>
2026-08-22 23:38:31 +0100

    libft: add ft_itoa

---
+++ libft/ft_itoa.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   ft_itoa.c                                           +:      :+:    :+:   */
+/*                                                       +:+ +:+         +:+  */
+/*   By: anwar <anwar@student.42.fr>                     +#+  +:+       +#+   */
+/*                                                       +#+#+#+#+#+   +#+    */
+/*   Created: 2026/08/22 23:38:31 by anwar               #+#+    #+#          */
+/*   Updated: 2026/08/22 23:38:31 by anwar               ###   ########.fr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	num_len(long n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	long	nb;
+	size_t	len;
+
+	nb = n;
+	len = num_len(nb);
+	str = (char *)malloc(len + 1);
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	if (nb < 0)
+		nb = -nb;
+	while (len--)
+	{
+		str[len] = (nb % 10) + '0';
+		nb /= 10;
+	}
+	if (n < 0)
+		str[0] = '-';
+	return (str);
+}
