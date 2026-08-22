037c9c70a0dce3f7a1967ef0706f6ddf31e7b249
Anwaroxxx <anwar.azarzou.6969@gmail.com>
2026-08-22 23:37:29 +0100

    libft: add ft_strnstr

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   ft_strnstr.c                                        +:      :+:    :+:   */
+/*                                                       +:+ +:+         +:+  */
+/*   By: anwar <anwar@student.42.fr>                     +#+  +:+       +#+   */
+/*                                                       +#+#+#+#+#+   +#+    */
+/*   Created: 2026/08/22 23:37:29 by anwar               #+#+    #+#          */
+/*   Updated: 2026/08/22 23:37:29 by anwar               ###   ########.fr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (*needle == '\0')
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (needle[j] != '\0' && i + j < len
+			&& haystack[i + j] == needle[j])
+			j++;
+		if (needle[j] == '\0')
+			return ((char *)&haystack[i]);
+		i++;
+	}
+	return (NULL);
+}
