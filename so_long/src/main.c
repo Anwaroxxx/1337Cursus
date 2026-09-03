c28fb238dba35232f11054b39af604764047a056
Anwaroxxx <anwar.azarzou.6969@gmail.com>
2026-09-03 20:15:23 +0100

    src/main.c: Initialize enemies and game loop hook
    - Add srand(time(NULL)) for enemy random movement
    - Call init_enemies() after init_images()
    - Register mlx_loop_hook for game_loop() (animations/enemies)
    - Change g_game from static to global for game_loop access

--- so_long/src/main.c
+++ so_long/src/main.c
@@ -1,6 +1,7 @@
 #include "so_long.h"
+#include <time.h>
 
-static t_game	*g_game = NULL;
+t_game	*g_game = NULL;
 
 static int	close_window_wrapper(void)
 {
@@ -11,6 +12,7 @@ int	main(int argc, char **argv)
 {
 	t_game	game;
 
+	srand(time(NULL));
 	ft_bzero(&game, sizeof(t_game));
 	if (argc != 2)
 		error_exit("Usage: ./so_long <map.ber>", &game);
@@ -20,10 +22,12 @@ int	main(int argc, char **argv)
 	validate_map(&game);
 	init_game(&game);
 	init_images(&game);
+	init_enemies(&game);
 	render_map(&game);
 	g_game = &game;
 	mlx_key_hook(game.win, key_hook, &game);
 	mlx_hook(game.win, 17, 0, close_window_wrapper, &game);
+	mlx_loop_hook(game.mlx, game_loop, NULL);
 	mlx_loop(game.mlx);
 	return (0);
 }
